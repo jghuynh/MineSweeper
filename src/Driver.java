fa72d62e9d6fb9ba731dcafb85b9073fc4285914
Justine Huynh <jgh75@georgetown.edu>
2021-01-30 16:38:15 -0500

    Fixed little bug to fit parameters

--- src/Driver.java
+++ src/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		int size = 9;
 		MineSweeperBoard board = new MineSweeperBoard(size);
-		OuterBoard outerBoard = new OuterBoard(size);
+		OuterBoard outerBoard = new OuterBoard(board);
 		
 		
 		// adds the bombs, also updates the boxes around the bombs
